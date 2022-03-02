38a9aac005069d9f7d02bddde14f589723a4a7d3
Dylan Edwards <dylan.edwards@dylan-edwards.local>
2022-03-02 12:03:59 -0800

    finish class to create songs

--- music-generator-api/LanguageGenerationLSTM.py
+++ music-generator-api/LanguageGenerationLSTM.py
@@ -1,13 +1,13 @@
 import tensorflow
 import random
-from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer
 # from keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Embedding
-from tensorflow.keras.utils import to_categorical
-from  tensorflow.keras.preprocessing import text_dataset_from_directory
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.layers import LSTM
+# from tensorflow.keras.layers import Embedding
+# from tensorflow.keras.utils import to_categorical
+# from  tensorflow.keras.preprocessing import text_dataset_from_directory
 import numpy
 from keras.preprocessing.sequence import pad_sequences
 import os
@@ -19,7 +19,10 @@ class LanguageGenerationLSTM:
         self.model = tensorflow.keras.models.load_model(pickle_file_path)
         self.songs_path = songs_path
         self.songs = []
+        self.tokenizer = Tokenizer()
+        self.max_length = 4
         self.collectSongs()
+        self.fitTokenizer()
 
     def importData(self, directory: str) -> list:
         with open(directory) as f:
@@ -30,16 +33,25 @@ class LanguageGenerationLSTM:
 
     def collectSongs(self):
         for file in self.getAllFilesInFolder(self.songs_path):
-            filePath = "/content/drive/MyDrive/song_data/Country/{}".format(file)
+            filePath = "{}/{}".format(self.songs_path, file)
             if "DS_Store" not in filePath:
                 self.songs.append(self.importData(filePath))
+
+    def fitTokenizer(self):
+        # integer encode text
+        self.tokenizer.fit_on_texts(self.songs)
+        vocab_size = len(self.tokenizer.word_index) + 1
+        print('Vocabulary Size: %d' % vocab_size)
+
     
-    def createSong(self):
+    def createSong(self) -> str:
+        song = ""
         for i in range(10):
-            print(generate_seq(test_model, tokenizer, max_length-1, getSentanceStarter(3, songs), 10))
+            song += self.generate_seq(self.model, self.tokenizer, self.max_length-1, self.getSentanceStarter(3, self.songs), 10) + "\n"
+        return song
 
 
-    def getSentanceStarter(sentanceStarterLenth, songs):
+    def getSentanceStarter(self, sentanceStarterLenth, songs):
         randomSongUpperBound = len(songs)
         #
         randomSongIndex = random.randint(0, randomSongUpperBound-1)
@@ -53,7 +65,7 @@ class LanguageGenerationLSTM:
 
 
         # generate a sequence from a language model
-    def generate_seq(model, tokenizer, max_length, seed_text, n_words):
+    def generate_seq(self, model, tokenizer, max_length, seed_text, n_words):
         in_text = seed_text
         # generate a fixed number of words
         for _ in range(n_words):
--- music-generator-api/main.py
+++ music-generator-api/main.py
@@ -1,3 +1,4 @@
+from LanguageGenerationLSTM import LanguageGenerationLSTM
 from bottle import Bottle, run
 
 app = Bottle()
@@ -8,6 +9,9 @@ def hello():
 
 if __name__ == '__main__':
     # load models 
+    rockModel = LanguageGenerationLSTM("./pickles/hackathon-model-rock", "./data/Rock")
+    song = rockModel.createSong()
+    print(song)
     
-    port = int(os.environ.get('PORT', 8080))
-    run(host='0.0.0.0', port=port, debug=True)
+    # port = int(os.environ.get('PORT', 8080))
+    # run(host='0.0.0.0', port=port, debug=True)
