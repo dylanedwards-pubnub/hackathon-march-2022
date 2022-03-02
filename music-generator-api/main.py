5efe269722a48acebb763979cbaab539472624ec
Dylan Edwards <dylan.edwards@dylan-edwards.local>
2022-03-02 12:15:01 -0800

    complete api

--- music-generator-api/main.py
+++ music-generator-api/main.py
@@ -1,17 +1,39 @@
+import os
 from LanguageGenerationLSTM import LanguageGenerationLSTM
 from bottle import Bottle, run
 
+rockModel = LanguageGenerationLSTM("./pickles/hackathon-model-rock", "./data/Rock")
+print("----- ROCK MODEL LOADED -----")
+popModel = LanguageGenerationLSTM("./pickles/hackathon-model-pop", "./data/Pop")
+print("----- POP MODEL LOADED -----")
+countryModel = LanguageGenerationLSTM("./pickles/hackathon-model-country", "./data/Country")
+print("----- COUNTRY MODEL LOADED -----")
+metalModel = LanguageGenerationLSTM("./pickles/hackathon-model-metal", "./data/Metal")
+print("----- METAL MODEL LOADED -----")
+
 app = Bottle()
 
-@app.route('/hello')
-def hello():
-    return "Hello World!"
+@app.route('/rock')
+def getRockSong():
+    song = rockModel.createSong()
+    return {"song": song, "genre": "Rock"}
+
+@app.route('/metal')
+def getMetalSong():
+    song = metalModel.createSong()
+    return {"song": song, "genre": "Metal"}
+
+@app.route('/country')
+def getCountrySong():
+    song = countryModel.createSong()
+    return {"song": song, "genre": "Country"}
+
+@app.route('/pop')
+def getPopSong():
+    song = popModel.createSong()
+    return {"song": song, "genre": "Pop"}
 
 if __name__ == '__main__':
-    # load models 
-    rockModel = LanguageGenerationLSTM("./pickles/hackathon-model-rock", "./data/Rock")
-    song = rockModel.createSong()
-    print(song)
-    
-    # port = int(os.environ.get('PORT', 8080))
-    # run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    print("----- STARTING SERVER ON PORT 5000 -----")
+    app.run(host='0.0.0.0', port=port, debug=True)
